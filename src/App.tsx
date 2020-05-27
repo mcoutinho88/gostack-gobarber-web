d09dbb3bf6125e07a05c7ae61f727f13758f1854
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2020-05-27 12:41:20 -0300

    Authenticated Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyles from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
-      <AuthProvider>
-        <SignIn />
-      </AuthProvider>
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+
       <GlobalStyles />
-    </>
+    </Router>
   );
 };
 export default App;
